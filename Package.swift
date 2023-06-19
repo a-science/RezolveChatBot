e80776d339ec9a8d76cb695b62e285a44e9ac3a4
Arun Madasamy <arungre@hotmail.com>
2023-06-19 13:35:08 +0530

    initial commit of code with package file

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "RezolveChatBot",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "RezolveChatBot",
+            targets: ["rezolve_chatbot"]),
+    ],
+    targets: [
+            .target(
+                name: "RezolveChatBot",
+                dependencies: [],
+                resources: [
+                            .process("Resources/index.html")
+                ]
+            )
+        ]
+)
+
+
+
+
